da9a235b2e3e59513ae2bbfcc587f2f3fa0c42b3
GGapa <ggapa@outlook.com>
2024-12-11 20:25:47 +0800

    Update template Math.cpp

--- template/Math/Math.cpp
+++ template/Math/Math.cpp
@@ -1,36 +1,33 @@
-namespace Math {
-    namespace pre {
-        template <typename T>
-        constexpr T qpow(T x, int y){
-            T ret = 1;
-            for(; y; y >>= 1) {
-                if(y & 1) (ret = ret * x) %= mod;
-                (x = x * x) %= mod;
-            }
-            return ret;
+namespace pre {
+    template <typename T>
+    constexpr T qpow(T x, int y){   // 因为使用了 template 所以进 qpow 的时候别忘记开 long long
+        T ret = 1;
+        for(; y; y >>= 1) {
+            if(y & 1) (ret = ret * x % mod);
+            (x = x * x % mod) ;
         }
- 
-        template <typename T>
-        constexpr T inv(T x) {return qpow(x, mod - 2); }
-    }   
-    using namespace pre;
- 
-    constexpr LL add(const LL x, const LL y) {return (x + y + mod) % mod; }
-    constexpr LL mul(const LL x, const LL y) {return (x * y % mod + mod) % mod; }
- 
+        return ret;
+    }
+
+    template <typename T>
+    constexpr T inv(T x) {return qpow(x, mod - 2); }
+}   
+using namespace pre;
+
+namespace binom {
+    constexpr int N = 1e6 + 5;
     LL fac[N], iv[N];
  
     void init() {
         fac[0] = 1;
-        rep(i, 1, N - 1) fac[i] = mul(fac[i - 1], i);
+        rep(i, 1, N - 1) fac[i] = fac[i - 1] * i % mod;
         iv[N - 1] = inv(fac[N - 1]); 
-        per(i, N - 2, 0) iv[i] = mul(iv[i + 1], i + 1) ;
+        per(i, N - 2, 0) iv[i] = iv[i + 1] * (i + 1) % mod;
     }
  
     LL C(int y, int x) {
         if(x < 0 || y < 0 || y - x < 0) return 0;
-        return mul(fac[y], mul(iv[x], iv[y - x])) ;
+        return fac[y] * iv[x] * iv[y - x];
     }   
 }
-using Math::C; 
-using namespace Math::pre;
+using binom::C; 
