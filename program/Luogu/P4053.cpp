613db6d33d1be0e313c9a1848b08389f1b57320e
GGapa <GGapa@outlook.com>
2024-12-14 23:04:12 +0800

    Update P4053.cpp

--- program/Luogu/P4053.cpp
+++ program/Luogu/P4053.cpp
@@ -30,7 +30,7 @@ signed main() {
     ios::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
     int T = 1;
-    cin >> T;
+    // cin >> T;
     while(T--) {
         solve();
         if(T) cout << '\n';
