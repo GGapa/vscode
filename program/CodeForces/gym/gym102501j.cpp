dc52aa0168ac4b4e8bf5d46ee10d15ed803bba08
GGapa <GGapa@outlook.com>
2024-12-15 14:16:17 +0800

    Update gym102501j.cpp

--- program/CodeForces/gym/gym102501j.cpp
+++ program/CodeForces/gym/gym102501j.cpp
@@ -10,20 +10,39 @@ constexpr int mod = 1e9 + 7;
 namespace catalan {
     constexpr int N = 1e6 + 5;
     LL iv[N], C[N];
-    iv[1] = 1;
-    rep(i, 2, N - 1) iv[i] = 1ll * (mod - mod / i) * iv[mod % i] % mod;
-    C[0] = C[1] = 1;
-    rep(i, 2, N - 2) C[i] = C[i - 1] * 2 * (2 * i - 1) % mod * iv[i + 1] % mod;
+    void init() {
+        iv[1] = 1;
+        rep(i, 2, N - 1) iv[i] = 1ll * (mod - mod / i) * iv[mod % i] % mod;
+        C[0] = C[1] = 1;
+        rep(i, 2, N - 2) C[i] = C[i - 1] * 2 * (2 * i - 1) % mod * iv[i + 1] % mod;
+    }
 }using catalan::C;
 
 signed main() {
     ios::sync_with_stdio(0);
-    cin.tie(0); cout.time(0);
+    cin.tie(0); cout.tie(0);
+    catalan::init();
     int n; cin >> n;
     vector<int> A(n + 1);
     rep(i, 1, n) cin >> A[i];
     
-    int mx = *max_element(A.begin() + 1, A.end())l;
-    vector<int> L(n + 1);
+    int mx = *max_element(A.begin() + 1, A.end());
+    vector<int> L(n + 1), F(mx + 1), stk(1, 0);
+    rep(i, 1, n) {
+        while(A[stk.back()] > A[i]) stk.pop_back();
+        L[i] = stk.back(); stk.emplace_back(i);
+    }
+
+    LL ans = 1;
+    rep(i, 1, n) {
+        int &f = F[A[i]];
+        if(A[L[i]] == A[i]) f++;
+        else (ans *= C[f]) %= mod, f = 1;
+    }
+
+    for(auto f : F) (ans *= C[f]) %= mod;
+
+    cout << ans << '\n';
+
     return 0;
 }
