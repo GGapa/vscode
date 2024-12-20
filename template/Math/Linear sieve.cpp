da711a0fc09ba9104d47a664b0082e96bfdbf82d
GGapa <ggapa@outlook.com>
2024-12-20 16:23:13 +0800

    Update Linear sieve.cpp

--- template/Math/Linear sieve.cpp
+++ template/Math/Linear sieve.cpp
@@ -1,16 +1,16 @@
 namespace LS {
     constexpr int N = 1e6 + 5;
-    int pri[N], phi[N], mi[N], mu[N];
+    int pri[N], phi[N], mn[N], mu[N];
     bitset<N> vis;
 
     void init(){
         mu[1] = 1;
         rep(i, 2, N - 1) {
-            if(!vis[i]) pri[++pri[0]] = i, mi[i] = i, phi[i] = i - 1, mu[i] = -1;
+            if(!vis[i]) pri[++pri[0]] = i, mn[i] = i, phi[i] = i - 1, mu[i] = -1;
             for(int j = 1; j <= pri[0] && i * pri[j] < N; j++) {
                 int tt = i * pri[j];
                 vis[tt] = 1;
-                mi[tt] = pri[j];
+                mn[tt] = pri[j];
                 if(i % pri[j] == 0) {
                     phi[tt]  = phi[i] * pri[j];
                     break;
@@ -20,4 +20,14 @@ namespace LS {
             }
         }
     }
-}
+
+    vector<pair<int, int>> getpri(int x) {
+        vector<pair<int, int>> ret;
+        while(x > 1) {
+            int t = mn[x], cnt = 0;
+            while(x % t == 0) x /= t, cnt++;
+            ret.emplace_back(t, cnt);
+        }
+        return ret;
+    } 
+}using LS::getpri;
