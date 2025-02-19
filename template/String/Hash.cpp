a86b4c0956a30d47a49b59f1a62d6580cefa55ba
GGapa <ggapa@outlook.com>
2025-02-19 14:51:50 +0800

    Add double module Hash

--- template/String/Hash.cpp
+++ template/String/Hash.cpp
@@ -11,4 +11,25 @@ struct Hash {
         return H[i] - H[j] * XP[j - i];
     }
 };
-KMP
+
+namespace Hash {
+    static constexpr int mod1 = 98244853, mod2 = 998442853;
+
+    void init() {
+        pw1[0] = pw2[0] = 1;
+        rep(i, 1, N - 1) pw1[i] = pw1[i - 1] * B % mod1, pw2[i] = pw2[i - 1] * B % mod2;
+    }
+    struct Hash {
+        LL x, y;
+        Hash(LL a = 0) {x = a % mod1, y = a % mod2; }
+        Hash(LL a, LL b) : x(a), y(b) {}
+        Hash operator+(const Hash &t) { return Hash((x + t.x) % mod1, (y + t.y) % mod2); }
+        Hash operator-(const Hash &t) { return Hash((x + mod1 - t.x) % mod1, (y + mod2 - t.y) % mod2); }
+        Hash operator*(const int &t) { return Hash(x * pw1[t] % mod1, y * pw2[t] % mod2); }
+        bool operator<(const Hash &t) const { return x == t.x ? y < t.y : x < t.x; }
+        bool operator==(const Hash &t) {return x == t.x && y == t.y; }
+    } H[N];
+
+    map<Hash, AI<2>> mp;
+    Hash get(int l, int r) {return H[r] - H[l - 1] * (r - l + 1); }
+}using namespace Hash;
